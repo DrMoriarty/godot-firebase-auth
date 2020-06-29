c22a1ade1a708791aeee703486425450f2ec0aa3
Vasiliy Makarov <drmoriarty.0@gmail.com>
2020-06-29 21:40:03 +0300

    v 0.0.2 for godot-3.2.2.stable

--- android-plugin/src/main/java/ru/mobilap/firebaseauth/FirebaseAuth.java
+++ android-plugin/src/main/java/ru/mobilap/firebaseauth/FirebaseAuth.java
@@ -81,7 +81,7 @@ public class FirebaseAuth extends GodotPlugin {
     }
 
     @Override
-    public View onMainCreateView(Activity activity) {
+    public View onMainCreate(Activity activity) {
         return null;
     }
 
